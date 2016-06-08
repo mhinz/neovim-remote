7dce7ea34fac2bebde5aec7c2a2746b6b5f24bee
Marco Hinz <mh.codebro@gmail.com>
2016-06-08 16:28:42 +0200

    Release 1.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires = ['neovim'],
     scripts          = ['bin/nvr'],
     packages         = [],
-    version          = '1.2',
+    version          = '1.2.1',
     license          = 'MIT',
     keywords         = 'neovim nvim nvr remote helper',
     classifiers      = [
