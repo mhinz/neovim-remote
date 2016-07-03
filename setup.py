7e76f845a3036b66affc200ef63d5758ec67d6f0
Marco Hinz <mh.codebro@gmail.com>
2016-07-03 13:24:54 +0200

    Remove leading -- from arguments list
    Fixes https://github.com/mhinz/neovim-remote/issues/17

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires = ['neovim'],
     scripts          = ['bin/nvr'],
     packages         = [],
-    version          = '1.2.1',
+    version          = '1.2.2',
     license          = 'MIT',
     keywords         = 'neovim nvim nvr remote helper',
     classifiers      = [
