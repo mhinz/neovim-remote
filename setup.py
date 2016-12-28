8d7fe39aee35b5bbc89597178aeb7c49deaf3765
Marco Hinz <mh.codebro@gmail.com>
2016-12-28 13:45:27 +0100

    -q: switch to correct directory first
    References
    https://github.com/mhinz/neovim-remote/issues/29

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires = ['neovim'],
     scripts          = ['bin/nvr'],
     packages         = [],
-    version          = '1.4.2',
+    version          = '1.4.3',
     license          = 'MIT',
     keywords         = 'neovim nvim nvr remote helper',
     classifiers      = [
