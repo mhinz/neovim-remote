020bb0ca34241bdae3b13bb5551cd6ba11ff019f
Marco Hinz <mh.codebro@gmail.com>
2017-03-19 16:48:00 +0100

    Fix --remote-wait-silent
    References #36.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires = ['neovim'],
     scripts          = ['bin/nvr'],
     packages         = [],
-    version          = '1.5.0',
+    version          = '1.5.1',
     license          = 'MIT',
     keywords         = 'neovim nvim nvr remote helper',
     classifiers      = [
