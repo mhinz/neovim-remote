2e4bf0f8365afabfc39e9d2be600dfa526b12a73
Marco Hinz <mh.codebro@gmail.com>
2018-03-12 17:59:16 +0100

    Test: introduce Nvim class

--- tests/test_nvr.py
+++ tests/test_nvr.py
@@ -6,16 +6,29 @@ import signal
 import subprocess
 import nvr
 
-def test_open_and_write_file():
-    env = {'NVIM_LISTEN_ADDRESS': '/tmp/pytest_nvimsock'}
-    env.update(os.environ)
+env = {'NVIM_LISTEN_ADDRESS': '/tmp/pytest_nvimsock'}
+testfile = '/tmp/pytest_file'
 
-    with subprocess.Popen(['nvim', '-nu', 'NORC', '--headless'], close_fds=True,
-                          env=env) as proc:
+
+class Nvim:
+    nvim = None
+
+    def start(self, env={}):
+        env.update(os.environ)
+        self.nvim = subprocess.Popen(['nvim', '-nu', 'NORC', '--headless'],
+                                close_fds=True, env=env)
         time.sleep(1)
-        argv = ['nvr', '-c', 'e /tmp/pytest_file | %d | exe "norm! iabc" | w']
-        nvr.main(argv=argv, env=env)
-        proc.send_signal(signal.SIGTERM)
 
+    def stop(self):
+        self.nvim.send_signal(signal.SIGTERM)
+
+
+def test_open_and_write_file():
+    nvim = Nvim()
+    nvim.start()
+    argv = ['nvr', '-c', 'e /tmp/pytest_file | %d | exe "norm! iabc" | w']
+    nvr.main(argv, env)
+    nvim.stop()
     with open('/tmp/pytest_file') as f:
         assert 'abc\n' == f.read()
+    os.remove(testfile)
