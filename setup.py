17cff732103cad0439b1eafd7ef834eb5d053ebb
Marco Hinz <mh.codebro@gmail.com>
2018-06-11 18:45:43 +0200

    Bump to 2.0.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'console_scripts': ['nvr = nvr.nvr:main']
     },
     packages         = ['nvr'],
-    version          = '2.0.5',
+    version          = '2.0.6',
     license          = 'MIT',
     keywords         = 'neovim nvim nvr remote helper',
     classifiers      = [
